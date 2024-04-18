f4794465f01d9e7b1ae43f3212c0ab18adb341c5
LamiaAslanova <tu826UPXV@code.edu.az>
2024-04-18 11:40:19 +0400

    hotel task

---
+++ ConsoleApp37/Hotel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37
+{
+    internal class Hotel
+    {
+
+        public string Name { get; set; }
+        private List<Room> Rooms { get; set; } = new List<Room>();
+
+        public Hotel(string name)
+        {
+            Name = name;
+        }
+
+        public void AddRoom(Room room)
+        {
+            Rooms.Add(room);
+        }
+
+        public void MakeReservation(int? roomId)
+        {
+            if (roomId == null)
+            {
+                throw new NullReferenceException("bele otaq yoxdur");
+            }
+
+             Rooms.Find(r => r.Id == roomId);
+            Room room = Rooms.Find(r => r.Id == roomId);
+
+
+
+        }
+
+    }
+}
+
---
+++ ConsoleApp37/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp37
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hotel hotel =new Hotel("Ayli Gece");
+            Room room1=new Room("orxan",15,15);
+            Room room2 = new Room("Kenann", 15, 15);
+            Room room3 = new Room("Resul", 15, 15);
+            hotel.AddRoom(room1);
+            hotel.AddRoom(room2);
+            hotel.AddRoom(room3);
+           
+            Console.WriteLine(room1.ShowInfo());
+            hotel.MakeReservation(2);
+        }
+    }
+}
